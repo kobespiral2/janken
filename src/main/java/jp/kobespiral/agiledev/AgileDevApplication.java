628885470bfd750a1604427558471f56f4968666
ssachio <sachio@SACHIO-DELL.localdomain>
2020-07-16 17:38:53 +0900

    first commit

---
+++ src/main/java/jp/kobespiral/agiledev/AgileDevApplication.java
@@ -0,0 +1,13 @@
+package jp.kobespiral.agiledev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileDevApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AgileDevApplication.class, args);
+  }
+
+}
